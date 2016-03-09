4b8642af6ace449890c31bf4abeef28675f9fc6d
sheep3 <xuxinhome@126.com>
2016-03-09 21:03:59 +0800

    Learning MyBatis!
    demo by  https://www.logcg.com/archives/961.html

---
+++ src_user/com/yihaomen/mybatis/inter/IUserOperation.java
@@ -0,0 +1,15 @@
+package com.yihaomen.mybatis.inter;
+
+import java.util.List;
+
+import com.yihaomen.mybatis.model.Article;
+import com.yihaomen.mybatis.model.User;
+
+public interface IUserOperation {
+	public User selectUserByID(int id);
+	public List<User> selectUsers(String userName);
+	public void addUser(User user);
+	public void updateUser(User user);
+	public void deleteUser(int id);
+	public List<Article> getUserArticles(int userid);
+}
